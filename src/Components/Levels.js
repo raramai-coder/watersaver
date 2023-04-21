472113a2ab1ce1a9ff6de00f07a3f3b0cf128cbc
raramai-coder <122776451+dev-nashe@users.noreply.github.com>
2023-04-21 23:57:25 +0200

    basic skeleton

--- src/Components/Levels.js
+++ src/Components/Levels.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "../Styles/main.css";
+import {
+	GoogleMapProvider,
+	useGoogleMap,
+} from "@ubilabs/google-maps-react-hooks";
 
 function Levels() {
-	return <div>This is the Levels page!</div>;
+	return (
+		<main>
+			<h1>Dam Levels</h1>
+			<div className="level-container">
+				<GoogleMapProvider googleMapsAPIKey></GoogleMapProvider>
+				<div className="map-area">Map ARea</div>
+				<div className="info-area">
+					<h2>Berg River</h2>
+					<p className="info-text-highlight">Storage Level: 79.57%</p>
+					<p>Last Week: 79.57%</p>
+					<p>Last Year: 25.44%</p>
+					<p>Reading Date: 10/3/2023</p>
+					<p>Search</p>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default Levels;
