27fe2da27005915d57cc4609d81ffb6e7461b51c
DianeBada <92454613+DianeBada@users.noreply.github.com>
2023-04-22 00:50:16 +0200

    report page done

--- src/Components/Report.js
+++ src/Components/Report.js
@@ -1,8 +1,100 @@
-import React from "react";
 import "../Styles/main.css";
+import "../report.css";
+import React, { useState } from 'react';
 
-function Report() {
-	return <div>This is the Report page!</div>;
+function ReportProblem() {
+  const [name, setName] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [location, setLocation] = useState('');
+  const [urgency, setUrgency] = useState('not urgent');
+  const [problem, setProblem] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleMobileChange = (event) => {
+    setMobile(event.target.value);
+  };
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleUrgencyChange = (event) => {
+    setUrgency(event.target.value);
+  };
+
+  const handleProblemChange = (event) => {
+    setProblem(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+    console.log('Name:', name);
+    console.log('Mobile:', mobile);
+    console.log('Location:', location);
+    console.log('Urgency:', urgency);
+    console.log('Problem:', problem);
+  };
+
+  return (
+    <div className="report-container">
+      <h1 className="report-title">Report</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Enter your name"
+            required
+          />
+        </label>
+        <label>
+          Mobile:
+          <input
+            type="tel"
+            value={mobile}
+            onChange={handleMobileChange}
+            placeholder="Enter your mobile number"
+            pattern="[0-9]{10}"
+            required
+          />
+        </label>
+        <label>
+          Location:
+          <input
+            type="text"
+            value={location}
+            onChange={handleLocationChange}
+            placeholder="Enter your location"
+            required
+          />
+        </label>
+        <label>
+          Urgency:
+          <select value={urgency} onChange={handleUrgencyChange}>
+            <option value="not urgent">Not Urgent</option>
+            <option value="medium">Medium</option>
+            <option value="emergency">Emergency</option>
+          </select>
+        </label>
+        <label>
+          Problem:
+          <textarea
+            value={problem}
+            onChange={handleProblemChange}
+            placeholder="Describe your problem"
+            required
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Report;
+export default ReportProblem;
