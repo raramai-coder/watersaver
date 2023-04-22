52117185844449d796863ddfd4591048b336c9bc
raramai-coder <122776451+dev-nashe@users.noreply.github.com>
2023-04-22 13:05:18 +0200

    google maps attempt

--- src/Components/Levels.js
+++ src/Components/Levels.js
@@ -1,24 +1,97 @@
-import React from "react";
 import "../Styles/main.css";
+import { Box, Input } from "@chakra-ui/react";
 import {
-	GoogleMapProvider,
-	useGoogleMap,
-} from "@ubilabs/google-maps-react-hooks";
+	useJsApiLoader,
+	GoogleMap,
+	Marker,
+	Autocomplete,
+} from "@react-google-maps/api";
+import Papa from "papaparse";
+import { useState } from "react";
+
+const center = {
+	lat: -25.6426590313842,
+	lng: 28.354841696486154,
+};
+
+const libraries = ["places"];
 
 function Levels() {
+	const [dams, setDams] = useState([]);
+
+	const getCsvData = async () => {
+		Papa.parse(await fetchCsv(), {
+			header: true,
+			skipEmptyLines: true,
+			complete: function (result) {
+				//console.log(result.data);
+				const copyData = [];
+				const dam = {
+					Dam: result.data.Dam,
+					Level: result.data.Level,
+					lat: result.data.lat,
+					lng: result.data.lng,
+				};
+				copyData.push(dam);
+				//setDams(result.data);
+				setDams(copyData);
+				//console.log(dams);
+				//dams.append(result);
+				//dams = result;
+				//console.log(dams);
+			},
+		});
+	};
+
+	const fetchCsv = async () => {
+		const response = await fetch("../Data/DemoDamLevels.csv");
+		let reader = response.body.getReader();
+		const result = await reader.read();
+		let decoder = new TextDecoder("utf-8");
+		const csv = await decoder.decode(result.value);
+		return csv;
+	};
+
+	getCsvData();
+
+	const { isLoaded } = useJsApiLoader({
+		googleMapsApiKey: process.env.NEXT_PUBLIC_MAP_API_KEY,
+		libraries: libraries,
+	});
+
+	if (!isLoaded) {
+		return <div className="map-area">Map ARea</div>;
+	}
+
 	return (
 		<main>
 			<h1>Dam Levels</h1>
 			<div className="level-container">
-				<GoogleMapProvider googleMapsAPIKey></GoogleMapProvider>
-				<div className="map-area">Map ARea</div>
+				<Box className="map-area">
+					<GoogleMap
+						center={center}
+						zoom={15}
+						mapContainerStyle={{ width: "100%", height: "100%" }}
+					>
+						{/* <Marker position={center} /> */}
+						{dams.map((lat, lng) => (
+							<Marker
+								position={{ lat: parseFloat(lat), lng: parseFloat(lng) }}
+							/>
+						))}
+					</GoogleMap>
+				</Box>
+
 				<div className="info-area">
 					<h2>Berg River</h2>
 					<p className="info-text-highlight">Storage Level: 79.57%</p>
 					<p>Last Week: 79.57%</p>
 					<p>Last Year: 25.44%</p>
 					<p>Reading Date: 10/3/2023</p>
-					<p>Search</p>
+
+					<Autocomplete>
+						<Input type="text" placeholder="Search for Dam"></Input>
+					</Autocomplete>
 				</div>
 			</div>
 		</main>
