211ab2e1b341623df26a99910a603ee6fb5a9bb6
Matipa Marufu <2344503@students.wits.ac.za>
2023-04-22 13:48:07 +0200

    LanguageSelector basic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home";
 import FAQ from "./Components/FAQ";
 import Report from "./Components/Report";
 import Levels from "./Components/Levels";
+import LanguageSelector from "./Components/LanguageSelector";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
@@ -18,6 +19,7 @@ function App() {
 				<Route path="faq" element={<FAQ />}></Route>
 				<Route path="report" element={<Report />}></Route>
 				<Route path="levels" element={<Levels />}></Route>
+				<Route path="language" element={<LanguageSelector />}></Route>
 			</Routes>
 		</>
 	);
